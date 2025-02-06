31b5aded9cab4f18dc4396e78039af520e867f24
pf55351 <francesco.primerano79@gmail.com>
2025-02-06 22:50:10 +0100

    Add new messages

---
+++ src/config/messages/contract-limit-messages.ts
@@ -0,0 +1,9 @@
+const CONTRACT_LIMIT_MESSAGES = {
+  NO_VALID_DATA_INIT: 'No valid data init found',
+  NO_VALID_DATA_END: 'No valid data end found',
+  NO_VALID_LIMIT: 'No valid tokenizations limit found',
+  MAX_TOKENIZATIONS_LIMIT_REACH: 'You have reached the maximum limit of tokenizations ',
+  MISSING_PARAMS: 'Missing parameters',
+};
+
+export { CONTRACT_LIMIT_MESSAGES };
