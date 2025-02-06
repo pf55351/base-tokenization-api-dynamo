06762dcecee5495307c9f17b6a9b0d70a4e3573e
pf55351 <francesco.primerano79@gmail.com>
2025-02-06 22:54:39 +0100

    Refactor

--- src/middlewares/check-expire.ts
+++ src/middlewares/check-expire.ts
@@ -1,18 +1,23 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { CONTRACT_EXPIRE_MESSAGES } from '@/config/messages/contract-expire-messages';
+import { IS_OFFLINE } from '@/config';
 import createError from 'http-errors';
+import { getSSMParameter } from '@/libs/aws/ssm';
 import middy from '@middy/core';
 
-const CONTRACT_EXPIRE_TS = Number(process.env.CONTRACT_EXPIRE_UNIX_TS);
+const envValue = process.env.CONTRACT_END_UNIX_TS;
 
 const checkExpire = (): middy.MiddlewareObj<APIGatewayProxyEvent, APIGatewayProxyResult> => {
   const before: middy.MiddlewareFn<APIGatewayProxyEvent, APIGatewayProxyResult> = async () => {
-    if (isNaN(CONTRACT_EXPIRE_TS)) {
+    if (!envValue) throw createError.BadRequest(CONTRACT_EXPIRE_MESSAGES.NO_VALID_DATA);
+    const contractEnd = IS_OFFLINE ? Number(envValue) : Number(await getSSMParameter(envValue));
+
+    if (isNaN(contractEnd)) {
       throw createError.BadRequest(CONTRACT_EXPIRE_MESSAGES.NO_VALID_DATA);
     }
     const currentTimestamp = Date.now();
 
-    if (currentTimestamp > CONTRACT_EXPIRE_TS) {
+    if (currentTimestamp > contractEnd) {
       throw createError(419, CONTRACT_EXPIRE_MESSAGES.CONTRACT_IS_EXPIRED);
     }
   };
